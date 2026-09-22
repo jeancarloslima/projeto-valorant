74cd1d72b2d1950f6c59f8f46f5b1291ca1e32ee
Jean Lima <jcoliveiralima19@gmail.com>
2026-09-22 12:18:30 -0300

    Definindo rotas e estrutura base do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
 
-  return (
-    <>
-    </>
-  )
+  useEffect(() => {
+    const fetchAgents = async () => {
+      const response = await axios.get("https://valorant-api.com/v1/agents");
+
+      console.log(response);
+    };
+
+    fetchAgents();
+  }, []);
+
+  return <></>;
 }
 
-export default App
+export default App;
